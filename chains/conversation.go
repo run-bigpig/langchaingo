92ae433a3ad673e0b661333470be4f44b6c6045d
syskey <syskeykala@gmail.com>
2025-02-08 18:08:18 +0800

    feat: 修改openai llm chat 流式响应的结果

--- chains/conversation.go
+++ chains/conversation.go
@@ -8,7 +8,7 @@ import (
 )
 
 //nolint:lll
-const _conversationTemplate = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+const _conversationTemplate = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know and don't start with AI: when replying.
 Current conversation:
 {{.history}}
 Human: {{.input}}
